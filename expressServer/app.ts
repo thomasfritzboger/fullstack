7995749353e2233a764101fd1125daa7d396f693
thomasfritzboger <thomasfritzboger@gmail.com>
2023-03-14 11:15:08 +0100

    minor bug fix to express server exercise

--- expressServer/app.ts
+++ expressServer/app.ts
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV=== 'development') {
 //     return uuidv4();
 //   };
 
-const getPeople = (): Person[] => {
+  const getPeople = (): Person[] => {
     const data = readFileSync('./people.json', 'utf-8');
     const parsedData = JSON.parse(data);
     return parsedData.people as Person[];
@@ -46,21 +46,25 @@ const findPersonById = (people: Person[], id: string): Person | undefined => {
   return people.find(person => person.id.toString() === id);
 };
 
-app.get('/api/people', (req: Request, res: Response): void => {
-  const people = getPeople();
-  res.status(200).json(people);
-});
+  app.get('/api/people', (req: Request, res: Response) => {
+    const people = getPeople();
+    console.log('hej');
+    
+    console.log(people);
+    
+    res.status(200).json(people);
+  });
 
-app.get('/api/people/:id', (req: Request, res: Response): void => {
-  const id = req.params.id;
-  const people = getPeople();
-  const person = findPersonById(people, id);
-  if (person) {
-    res.status(200).json(person);
-  } else {
-    res.status(404).json({ msg: 'Person not found' });
-  }
-});
+  app.get('/api/people/:id', (req: Request, res: Response): void => {
+    const id = req.params.id;
+    const people = getPeople();
+    const person = findPersonById(people, id);
+    if (person) {
+      res.status(200).json(person);
+    } else {
+      res.status(404).json({ msg: 'Person not found' });
+    }
+  });
 
 app.post('/api/people', (req: Request, res: Response) : void => {
   const { name, age, city } = req.body;
@@ -74,7 +78,8 @@ app.post('/api/people', (req: Request, res: Response) : void => {
     city,
   };
   people.push(newPerson);
-  writeFileSync('./people.json', JSON.stringify(people,null,2));
+  writeFileSync('./people.json', JSON.stringify({ people }, null, 2));
+
   res.status(201).json(newPerson);
 });
   
@@ -93,7 +98,8 @@ app.post('/api/people', (req: Request, res: Response) : void => {
     city: req.body.city
   };
   people[personIndex] = updatedPerson;
-  writeFileSync('./people.json', JSON.stringify(people, null, 2));
+  writeFileSync('./people.json', JSON.stringify({ people }, null, 2));
+
   res.status(200).json(updatedPerson);
 });
 
@@ -109,19 +115,21 @@ app.patch('/api/people/:id', (req: Request, res: Response): void => {
     }
 
     people[index] = { ...people[index], ...updateData };
-    writeFileSync('./people.json', JSON.stringify(people, null, 2));
+    writeFileSync('./people.json', JSON.stringify({ people }, null, 2));
+
     res.status(200).json(people[index]);
   });
   
-app.delete('/api/people/:id', (req, res) => {
-  const people = getPeople();
-  const index = people.findIndex(person => person.id === req.params.id);
-  if (index === -1) {
-    return res.status(404).send('Person not found');
-  }
-  people.splice(index, 1);
-  writeFileSync('./people.json', JSON.stringify(people, null, 2));
-  res.json(people);
-});
+  app.delete('/api/people/:id', (req, res) => {
+    const people = getPeople();
+    const index = people.findIndex(person => person.id === req.params.id);
+    if (index === -1) {
+      return res.status(404).send('Person not found');
+    }
+    people.splice(index, 1);
+    writeFileSync('./people.json', JSON.stringify({ people }, null, 2));
+
+    res.json(people);
+  });
   
 app.listen(3001,() => {console.log(`server is listening to port 3001`)})
